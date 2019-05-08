9fd2fcdf534bbb03b9a3f8c92efcda91801a8f57
Okato <316981067@qq.com>
2019-05-08 13:39:37 +0800

    commit

---
+++ filestate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  path='.'
+elif [ $# -ne 1 ]; then
+  echo "usage: $0 basepath"
+elif [ ! -e $1 ]; then
+  echo "invalid basepath: $1"
+else
+  path=$1
+fi
+
+declare -A opStorage;
+
+while read line;
+do
+  ftype=`file -b "$line" | cut -d, -f1`;
+  let opStorage["$ftype"]++;
+done < `find $path -type f -print`
+echo ${!opStorage[*]}
+echo ============= File types and counts ============= 
+echo $i
+for ftype in "${!opStorage[@]}";
+do
+  echo $ftype : ${opStorage["$ftype"]}
+done
+
