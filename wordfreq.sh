cf07134b64894699c5eba7ef2ec35358e390929a
wengjiadong <316981067@qq.com>
2019-05-09 21:58:57 +0800

    word frequency count in file:

---
+++ wordfreq.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 filename"; exit
+elif [ -e $1 ] && [ -f $1 ]; then
+  filename=$1; echo "File: $filename"
+else
+  echo "Invalid file name"
+fi  
+
+egrep -o "\b[a-zA-Z]+\b" $filename | \
+  awk ' { count[$0]++ }
+    END { printf("%-14s %s\n", "Word", "Count") ;
+      for ( i in count )
+        { printf("%-14s %s\n", i, count[i]); } } '
