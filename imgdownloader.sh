d98256f6972d5d74b1434e029d1be1d7ae1e1f5a
wengjiadong <316981067@qq.com>
2019-05-10 22:25:06 +0800

    update

--- imgdownloader.sh
+++ imgdownloader.sh
@@ -7,13 +7,16 @@ fi
 while [ $# -gt 0 ] 
 do
   case $1 in
-  -d)   shift; directory=$1; shift ;;
-  *)    url=$1; shift ;;
+    -d)   shift; directory=$1; shift ;;
+    *)    url=$1; shift ;;
+  esac
 done
 
 mkdir -p $directory;
 baseurl=$(echo $url | grep -oE "https?://[a-z.\-]+")
 
-echo Dowloading $url
+echo Downloading $url
 curl -s $url | grep -oE "<img[^>]*src=[^>]*>" | \
-  sed 's/<img[^>]*src=\"\([^>]*\).*/\1/g | \
+  sed 's/<img[^>]*src=\"\{0,1\}\([^> "]*\).*/\1/g' | \
+  sed "s|$url|www123com|" > /tmp/$$.download.list.bak 
+
