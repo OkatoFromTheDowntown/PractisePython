04c44ffd75557650fcf1c4d5805f2907aa74b4e4
Okato <okato@localhost.localdomain>
2019-08-10 16:08:18 +0900

    update

--- bash-sh/myScript.sh
+++ bash-sh/myScript.sh
@@ -1,2 +1,8 @@
-#!/bash/bash
+#!/bin/bash
 
+# echoes "hello world"
+echo "hello world"
+echo 'hello world !'
+echo -e "1\t2\t3"
+echo -e "\e[1;31m This is red text \e[0m"
+echo -e "\e[1;46m This is my background \e[0m"
